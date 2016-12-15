ba9f6f8c0003764ef1b70912c7582bd139350fd9
FrankieMFerreira <FrankieMFerreira@gmail.com>
2016-12-15 11:28:23 -0500

    Hi

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Frankie!"
+  end
+
+  get '/hometown' do
+    "My hometown is New York City!"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is All I Want For Christmas is You(Buy it on iTunes)!"
+  end
+
+
 end
